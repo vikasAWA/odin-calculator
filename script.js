452fe405508c9ac8b07e4116bb95bb5e0ebaff06
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-08-08 19:17:16 +0530

    Added an operate function

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function divide(x, y) {
     return x / y;
 }
 
+function operate(operation, x, y) {
+    switch(operation) {
+        case "+":
+            return add(x, y);
+
+        case "-":
+            return subtract(x, y);
+
+        case "X":
+            return multiply(x, y);
+
+        case "/":
+            return divide(x, y);
+    }
+}
+
