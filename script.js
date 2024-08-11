563915c45ec5139aae2a602cd87871fecaad996a
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-08-11 14:17:30 +0530

    Added event-listeners to buttons
    Added event-listeners to buttons and declared variables
    to hold the first and second numbers and operator,
    Called the operate() function as "=" is clicked and
    displayed the solution on display screen.

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function divide(x, y) {
     return x / y;
 }
 
-let firstNumber, secondNumber, ops;
 
+// Defining function to calculate the expression.
 function operate(operation, x, y) {
     switch(operation) {
         case "+":
@@ -32,14 +32,60 @@ function operate(operation, x, y) {
     }
 }
 
+//Selecting the display element to work it with.
 const display = document.querySelector("#display");
-
+// Selecting all the buttons.
 const buttons = document.querySelectorAll('button');
 
+//Declaring variable to hold the numbers and operator.
+let firstNumber = '';
+let secondNumber = '';
+let ops = '';
+
+//Declaring a variable to hold display value.
+let displayValue;
 
 buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        display.textContent = e.target.textContent;
+    button.addEventListener("click", (e)=> {
+        // populate the screen when clicked number is printed ont he screen 
+        if (display.textContent === "0") {
+            display.textContent = e.target.textContent;
+        }
+        else {
+            display.textContent += e.target.textContent;
+        }
+        //when operation is clicked , save the number before it in firstNumber
+        if (["+", "-", "*", "/"].includes(e.target.textContent)) {
+            displayValue = display.textContent;
+            firstNumber = displayValue.slice(0, displayValue.length-1);
+            ops = e.target.textContent;
+
+        }
+        //As "=" is clicked
+        if (e.target.textContent === "=") {
+            displayValue = display.textContent;
+            displayValue = displayValue.slice(0, displayValue.length-1);
+            // Splitted the display string on operation
+            //saved the operation in ops variable
+            let displayArray = displayValue.split(ops);
+            //Saved the last element (second number) of the array in secondNumber variable.
+            secondNumber = displayArray[displayArray.length-1];
+            // Calculate the solution using operate()
+            let solution = operate(ops, +firstNumber, +secondNumber);
+            //displayed the solution into the display screen.
+            display.textContent = solution;
+        }
+
     })
 })
+
+
+
+
+
+
+
+
+
+
 
