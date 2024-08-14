a48e193e671352c54edd72148c3e60dd94535f7d
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-08-14 23:07:29 +0530

    fix some bugs

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function operate(opt, x, y) {
 let firstNumber = '';
 let secondNumber = '';
 let operator = null;
+let opCount = 0;
+let solutionFlag = false;
 
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
@@ -25,15 +27,52 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         let buttonText = e.target.textContent;
+        
         if (buttonText === "AC") {
             display.textContent = "0";
             firstNumber = "";
             secondNumber = "";
             operator = null;
+            opCount = 0;
         }
+        else if (["+", "-", "/", "*", "="].includes(buttonText)) {
+            opCount++;
+            if (buttonText !== "=" && opCount === 1) {
+                operator = buttonText;
+            }
+            display.textContent += buttonText;
+            let regex = /\d{1,10}(\+|-|\*|\/)\d{1,10}/g;
+            if (display.textContent.match(regex) && buttonText === "=") {
+                let displayValue = display.textContent;
+                displayValue = displayValue.slice(0, displayValue.length-1);
+                let displayStringArray = displayValue.split(operator);
+                firstNumber = parseFloat(displayStringArray[0]);
+                secondNumber = parseFloat(displayStringArray[1]);
+
+                let solution = operate(operator, firstNumber, secondNumber).toFixed(4);
+                display.textContent = solution;
+
+                // if (solution) {
+                //     solutionFlag = true;
+                //     firstNumber = solution;
+                //     operator = null;
+                // }
+
+            }
+            
+            if (opCount >= 2 && secondNumber === '' || buttonText === "=" && secondNumber === '') {
+                display.textContent = display.textContent.slice(0, display.textContent.length-1);
+                opCount = 0;
+            }
+            
+        }
+
         else {
-            let displayText = display.textContent === '0' ? buttonText : display.textContent + buttonText;
-            display.textContent = displayText;
+            if (solutionFlag === false) {
+                let displayText = display.textContent === '0' ? buttonText : display.textContent + buttonText;
+                display.textContent = displayText;
+            }
+
         }
         
     })
