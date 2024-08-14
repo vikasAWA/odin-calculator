1ecaf6a567ce55e39c347382bb0c3069dc7a5678
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-08-14 13:57:44 +0530

    rewriting the code

--- script.js
+++ script.js
@@ -1,114 +1,41 @@
-function add(x, y) {
-    return x + y;
-}
-
-function subtract(x, y) {
-    return x - y;
-}
-
-function multiply(x, y) {
-    return x * y
-}
-
-function divide(x, y) {
-    return x / y;
-}
-
-
-// Defining function to calculate the expression.
-function operate(operation, x, y) {
-    switch(operation) {
+function operate(opt, x, y) {
+    switch (opt) {
         case "+":
-            return add(x, y);
-
+            return x + y;
         case "-":
-            return subtract(x, y);
-
+            return x -y;
         case "*":
-            return multiply(x, y);
-
+            return x * y;
         case "/":
-            return divide(x, y);
+            return x / y;
+        default:
+            return y;
     }
 }
 
-//Selecting the display element to work it with.
-const display = document.querySelector("#display");
-// Selecting all the buttons.
-const buttons = document.querySelectorAll('button');
-
-//Declaring variable to hold the numbers and operator.
 let firstNumber = '';
 let secondNumber = '';
-let ops = '';
+let operator = null;
 
-//Declaring a variable to hold display value.
-let displayValue;
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
 
-//Clear feature
-function clear(event) {
-    if (event.target.textContent === 'AC') {
-        display.textContent = "0";
-    }
-}
 
-// delete feature
-function del(event) {
-    if (event.target.textContent === "DEL") {
-        display.textContent = display.textContent.slice(0, display.textContent.length -4);
-    }
-}
 
-let eventCount = 0;
-
-buttons.forEach((button) => {
+buttons.forEach(button => {
     button.addEventListener("click", (e) => {
-        eventCount += 1;
-        if (eventCount === 1 && !isNaN(e.target.textContent) ) {
-            display.textContent = e.target.textContent;
+        let buttonText = e.target.textContent;
+        if (buttonText === "AC") {
+            display.textContent = "0";
+            firstNumber = "";
+            secondNumber = "";
+            operator = null;
         }
-        else if (e.target.textContent === ".") {
-            display.textContent = "0."
+        else {
+            let displayText = display.textContent === '0' ? buttonText : display.textContent + buttonText;
+            display.textContent = displayText;
         }
         
-        else {
-            display.textContent += e.target.textContent;
-            if (["+", "-", "/", "*"].includes(e.target.textContent)) {
-                ops = e.target.textContent;
-                ["+", "-", "/", "*"].forEach((item) => {
-                    if (e.target.textContent === item) {
-                        e.target.disabled = true;
-                    }
-                } )
-                
-            }
-            if (e.target.textContent === "=") {
-                displayValue = display.textContent.slice(0, display.textContent.length-1);
-                let displayArray = displayValue.split(ops);
-                firstNumber = displayArray[0];
-                secondNumber = displayArray[1];
-                let solution = operate(ops, +firstNumber, +secondNumber);
-                display.textContent = solution;
-            }
-            
-        }
-
     })
 })
-
-//weird behaviors 
-//1. After the solution display the clicked number keep concating with soluion.
-//2. After solution if "=" is clicked it keep adding solution with firstNumber
-//3. Even in starting operations can be displayed in screen.
-//4.if 0, it does not take operations 
-
-
-
-
-
-
-
-
-
-
 
